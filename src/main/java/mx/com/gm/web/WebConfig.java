61fb9726ca1f1b7e842079e25d02216c2cfb953d
VladimirMorote <vldmrmorote@gmail.com>
2023-10-22 11:24:06 -0300

    commit 3

---
+++ src/main/java/mx/com/gm/web/WebConfig.java
@@ -0,0 +1,19 @@
+package mx.com.gm.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("en"));
+        return slr;
+    }
+}
