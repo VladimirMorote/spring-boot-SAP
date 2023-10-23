e31f460a222c8227f5b2f75b2dff469d889eb168
VladimirMorote <vldmrmorote@gmail.com>
2023-10-22 22:24:19 -0300

    commit 4

--- src/main/java/mx/com/gm/domain/Usuario.java
+++ src/main/java/mx/com/gm/domain/Usuario.java
@@ -1,31 +1,28 @@
 package mx.com.gm.domain;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.*;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 
-@Data
 @Entity
-@Table(name = "persona")
-public class Persona implements Serializable{
-    
+@Data
+@Table(name="usuario")
+public class Usuario implements Serializable{
     private static final long serialVersionUID = 1L;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idPersona;
+    private Long idUsuario;
     
     @NotEmpty
-    private String nombre;
+    private String username;
     
     @NotEmpty
-    private String apellido;
+    private String password;
     
-    @NotEmpty
-    @Email
-    private String email;
-    
-    private String telefono;
+    @OneToMany
+    @JoinColumn(name="id_usuario")
+    private List<Rol> roles;
 }
--- src/main/java/mx/com/gm/web/ControladorInicio.java
+++ src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -1,39 +1,39 @@
 package mx.com.gm.web;
 
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import mx.com.gm.domain.Persona;
-import mx.com.gm.service.PersonaService;
-
+import mx.com.gm.servicio.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
-
+    
     @Autowired
     private PersonaService personaService;
-
+    
     @GetMapping("/")
-    public String inicio(Model model){
+    public String inicio(Model model, @AuthenticationPrincipal User user){
         var personas = personaService.listarPersonas();
         log.info("ejecutando el controlador Spring MVC");
+        log.info("usuario que hizo login:" + user);
         model.addAttribute("personas", personas);
         return "index";
     }
-
+    
     @GetMapping("/agregar")
     public String agregar(Persona persona){
         return "modificar";
     }
-
+    
     @PostMapping("/guardar")
     public String guardar(@Valid Persona persona, Errors errores){
         if(errores.hasErrors()){
@@ -42,18 +42,17 @@ public class ControladorInicio {
         personaService.guardar(persona);
         return "redirect:/";
     }
-
+    
     @GetMapping("/editar/{idPersona}")
     public String editar(Persona persona, Model model){
         persona = personaService.encontrarPersona(persona);
         model.addAttribute("persona", persona);
         return "modificar";
     }
-
-    @GetMapping("/eliminar/{idPersona}")
+    
+    @GetMapping("/eliminar")
     public String eliminar(Persona persona){
         personaService.eliminar(persona);
         return "redirect:/";
     }
-
 }
